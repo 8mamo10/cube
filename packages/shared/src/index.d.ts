a65fa902dda08aa70685a07435c5e387661b9968
Hiroki Yamamoto <562105+8mamo10@users.noreply.github.com>
2026-02-28 19:25:43 +0700

    Remove old files

--- packages/shared/src/index.d.ts
+++
@@ -1,6 +0,0 @@
-export * from './types/user.types';
-export * from './types/card.types';
-export * from './types/stamp.types';
-export * from './types/qr.types';
-export * from './types/api.types';
-//# sourceMappingURL=index.d.ts.map
